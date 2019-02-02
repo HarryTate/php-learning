db8852b2043cc40dd2c3e0ba120659c649017abb
HarryTate <harryjamestate@gmail.com>
2019-02-02 22:16:17 +0000

    add files

---
+++ form.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<title>PHP Learning</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- <link rel="stylesheet" type="text/css" media="screen" href="main.css" /> -->
+<!-- <script src="main.js"></script> -->
+</head>
+<body>
+<form action="form.php" method="get">
+Name: <input type="text" name="name">
+Age: <input type="number" name="cat">
+<input type="submit" name="form_posted">
+</form>
+<br>
+<?php 
+if(isset($_POST['form_posted'])) {
+  echo "Your name is:" . $_GET["name"];
+  echo "Your name is:" . $_GET["cat"];
+}
+
+
+?>
+</body>
+</html>
