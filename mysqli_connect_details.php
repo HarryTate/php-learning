61d3f8f8e25b603c128e299e9b057307ed4b4406
HarryTate <harryjamestate@gmail.com>
2019-03-20 21:49:56 +0000

    Added a form that inserts data into the database

---
+++ mysqli_connect_details.php
@@ -0,0 +1,15 @@
+<!--
+     This does not contain real password or username and is only intended to show what is contained in the real connection file -->
+<?php
+$servername = Server Here;
+$username = Username hare;
+$password = Password Here;
+$dbname = Database Name Here;
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+?>
