32a4d77db7b193f11b4203c5fed85feef9bc58f8
HarryTate <harryjamestate@gmail.com>
2019-07-28 21:08:56 +0100

    More work on pdo

--- pdo/index.php
+++ pdo/index.php
@@ -34,6 +34,7 @@ $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
 // FETCH MULTIPLE POSTS
 
 // User Input
+$id = 1;
 $author = 'Harry';
 $is_published = true;
 // Postional Params
@@ -44,11 +45,46 @@ $is_published = true;
 // $posts = $stmt->fetchAll();
 
 // Named params
-$sql = 'SELECT * FROM posts WHERE author = :author && is_published = :is_published';
-$stmt = $pdo->prepare($sql);
-$stmt->execute(['author' => $author, 'is_published' => $is_published]);
-$posts = $stmt->fetchAll();
+// $sql = 'SELECT * FROM posts WHERE author = :author && is_published = :is_published';
+// $stmt = $pdo->prepare($sql);
+// $stmt->execute(['author' => $author, 'is_published' => $is_published]);
+// $posts = $stmt->fetchAll();
 
-foreach ($posts as $post) {
-    echo $post->title . "<br>";
-}
+// foreach ($posts as $post) {
+//     echo $post->title . "<br>";
+// }
+
+// FETCH SINGLE POST
+// $sql = 'SELECT * FROM posts WHERE id = :id';
+// $stmt = $pdo->prepare($sql);
+// $stmt->execute(['id' => $id]);
+// $post = $stmt->fetch();
+
+// echo $post->title;
+// echo $post->body;
+
+// GET ROW COUNT
+$stmt = $pdo->prepare('SELECT * FROM posts WHERE author = ?');
+$stmt->execute([$author]);
+// Counts number of rows
+$postCount = $stmt->rowCount();
+echo $postCount;
+
+// INSERT DATA
+// $title = 'Post five';
+// $body = 'This is the fifth post';
+// $author = 'Kevin';
+
+// $sql = 'INSERT INTO posts(title, body, author) VALUES(:title, :body, :author)';
+// $stmt = $pdo->prepare($sql);
+// $stmt->execute(['title' => $title, 'body' => $body, 'author' => $author]);
+// echo 'Post Added';
+
+// UPDATE DATA
+$id = 1;
+$body = 'This is the updated post';
+
+$sql = 'UPDATE posts SET body = :body WHERE id = :id';
+$stmt = $pdo->prepare($sql);
+$stmt->execute(['id' => $id, 'body' => $body]);
+echo 'Post updated';
